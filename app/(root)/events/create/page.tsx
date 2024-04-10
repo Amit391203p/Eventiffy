818f7cd88489dbcc9a4c499db6e299a34fb0844a
Amit <phogatamit004@gmail.com>
2024-04-10 18:23:43 +0530

    fixed bugs

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,5 +1,5 @@
-import EventForm from "@/components/shared/EventForm"
-import { auth } from "@clerk/nextjs";
+import EventForm from '@/components/shared/EventForm';
+import { auth } from '@clerk/nextjs';
 
 const CreateEvent = () => {
   const { sessionClaims } = auth();
@@ -8,15 +8,15 @@ const CreateEvent = () => {
 
   return (
     <>
-      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
+      <section className="wrapper bg-primary-50 bg-dotted-pattern bg-cover bg-center">
+        <h3 className="h5-bold text-center">Create Event</h3>
       </section>
 
-      <div className="wrapper my-8">
+      <div className="wrapper">
         <EventForm userId={userId} type="Create" />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default CreateEvent
+export default CreateEvent;
